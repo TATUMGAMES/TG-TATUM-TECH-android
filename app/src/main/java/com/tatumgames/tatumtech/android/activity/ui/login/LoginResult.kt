3fef2dc46af66217522f6853188c64b4eb70f4dd
Antwain <antwainhenry17@gmail.com>
2025-05-02 18:33:26 -0700

    Fix contentDescription warning and accessibility issues

---
+++ app/src/main/java/com/tatumgames/tatumtech/android/activity/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.tatumgames.tatumtech.android.activity.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
